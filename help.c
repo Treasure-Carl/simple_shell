7566ceb3d470b96550d246cf33e51c704537d07f
Treasure-carl <treasureolayinkapaschal@gmail.com>
2023-11-15 09:14:34 -0800

    Made some betty compliant

--- help.c
+++ help.c
@@ -1,20 +1,33 @@
 #include "shell.h"
 
+/*
+ * _shell_help - helps with instructions
+ *
+ * Return: help on success
+ */
 
-int shell_help(char **args __attribute__((unused))) {
-  int i;
-  printf("This is simple C shell build by Treasure Olayinka\n");
-  printf("Type program names and arguments, and hit enter.\n");
-  printf("The following are built in:\n");
+int shell_help(char **args __attribute__((unused)))
+{	int i;
+	printf("This is simple C shell build by Treasure Olayinka\n");
+	printf("Type program names and arguments, and hit enter.\n");
+	printf("The following are built in:\n");
 
-  for (i = 0; i < shell_builtins(); i++) {
-    printf(" %s\n", built_in_string[i]);
-  }
+	for (i = 0; i < shell_builtins(); i++)
+	{
+		printf(" %s\n", built_in_string[i]);
+	}
 
-  printf("Use the man command for information on other programs.\n");
-  return 1;
+	printf("Use the man command for information on other programs.\n");
+	return (1);
 }
 
-int shell_exit(char **args __attribute__((unused))) {
-  return 0;
+/*
+ * _shell_exit - exits the shell
+ *
+ * Return: (1) on success
+ */
+
+int shell_exit(char **args __attribute__((unused)))
+{
+	return (0);
 }
--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include "shell.h"
 
-int main(int argc __attribute__((unused)), char **argv __attribute__((unused))) {
-  shell_loop();
-  return EXIT_SUCCESS;
+/*
+ * _main - functions as the main progran
+ * @args: takes in no arguement
+ * Return: (0)
+ */
+
+int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
+{
+	shell_loop();
+	return (EXIT_SUCCESS);
 }
