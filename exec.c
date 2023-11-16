3d3327419ca87ffc3dc3af3cf105787cbc7fa716
Treasure-carl <treasureolayinkapaschal@gmail.com>
2023-11-16 15:56:40 -0800

    betty be giving unnecessary ish

--- exec.c
+++ exec.c
@@ -1,17 +1,20 @@
 #include "shell.h"
 
-int shell_execute(char **args) {
-  int i;
+int shell_execute(char **args)
+{
+	int i;
 
-  if (args[0] == NULL) {
-    return 1;
-  }
+	if (args[0] == NULL)
+	{
+		return (1);
+	}
+	for (i = 0; i < shell_builtins(); i++)
+	{
+		if (strcmp(args[0], built_in_string[i]) == 0)
+		{
+			return ((*built_in_function[i])(args));
+		}
+	}
 
-  for (i = 0; i < shell_builtins(); i++) {
-    if (strcmp(args[0], built_in_string[i]) == 0) {
-      return (*built_in_function[i])(args);
-    }
-  }
-
-  return shell_launch(args);
+	return (shell_launch(args));
 }
