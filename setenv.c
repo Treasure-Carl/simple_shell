d10aaddf03d59f919813646da9464affcd5b5c3e
Treasure-carl <treasureolayinkapaschal@gmail.com>
2023-11-16 15:47:11 -0800

    Handeled all trailing whitespace errors

--- setenv.c
+++ setenv.c
@@ -1,12 +1,18 @@
 #include "shell.h"
 
-int shell_setenv(char **args) {
-  if (args[1] == NULL || args[2] == NULL) {
-    fprintf(stderr, "shell: expected 2 arguments to \"setenv\"\n");
-  } else {
-    if (setenv(args[1], args[2], 1) != 0) {
-      perror("shell");
-    }
-  }
-  return 1;
+int shell_setenv(char **args)
+{
+	if (args[1] == NULL || args[2] == NULL)
+	{
+		fprintf(stderr, "shell: expected 2 arguments to \"setenv\"\n");
+	}
+	else
+	{
+		if (setenv(args[1], args[2], 1) != 0)
+		{
+			perror("shell");
+		}
+	}
+
+	return (1);
 }
--- split.c
+++ split.c
@@ -1,39 +1,42 @@
 #include "shell.h"
 
-char **shell_split_line(char *line) {
-  int bufsize = shell_TOK_BUFSIZE, position = 0;
-  char **tokens = malloc(bufsize * sizeof(char*));
-  char *token, **tokens_backup;
-  char* comment_start; /* Move declaration to the top of the function */
+char **shell_split_line(char *line)
+{
+	int bufsize = shell_TOK_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token, **tokens_backup;
+	char *comment_start; /* Move declaration to the top of the function */
 
-  if (!tokens) {
-    fprintf(stderr, "shell: allocation error\n");
-    exit(EXIT_FAILURE);
-  }
+	if (!tokens)
+	{
+		fprintf(stderr, "shell: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	/* Ignore everything after # */
+	comment_start = strchr(line, '#'); /* Now this is not a mixed declaration */
 
-  /* Ignore everything after # */
-  comment_start = strchr(line, '#'); /* Now this is not a mixed declaration */
-  if (comment_start) *comment_start = '\0';
+	if (comment_start)
+	*comment_start = '\0';
+	token = strtok(line, shell_TOK_DELIM);
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
 
-  token = strtok(line, shell_TOK_DELIM);
-  while (token != NULL) {
-    tokens[position] = token;
-    position++;
-
-    if (position >= bufsize) {
-      bufsize += shell_TOK_BUFSIZE;
-      tokens_backup = tokens;
-      tokens = realloc(tokens, bufsize * sizeof(char*));
-      if (!tokens) {
-        free(tokens_backup);
-        fprintf(stderr, "shell: allocation error\n");
-        exit(EXIT_FAILURE);
-      }
-    }
-
-    token = strtok(NULL, shell_TOK_DELIM);
-  }
-
-  tokens[position] = NULL;
-  return tokens;
+		if (position >= bufsize)
+		{
+			bufsize += shell_TOK_BUFSIZE;
+			tokens_backup = tokens;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				free(tokens_backup);
+				fprintf(stderr, "shell: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, shell_TOK_DELIM);
+	}
+	tokens[position] = NULL;
+	return (tokens);
 }
